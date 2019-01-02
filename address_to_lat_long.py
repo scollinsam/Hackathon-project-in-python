c1e2b8ea42e265bdf8f512f65a0d1ed7bbbf686d
Elliot Weissberg <elliot.weissberg@gmail.com>
2019-01-02 20:09:16 +0200

    Trying to debug

--- address_to_lat_long.py
+++ address_to_lat_long.py
@@ -1,4 +1,5 @@
 from opencage.geocoder import OpenCageGeocode
+from pprint import pprint
 
 def address_to_coordinates(address):
 
@@ -15,3 +16,13 @@ def address_to_coordinates(address):
     longitude = results[0]['geometry']['lng']
 
     return latitude, longitude
+
+def coordinates_to_address(latitude, longitude):
+
+    key = '4745929c7204418da8d7bd0b0b693a69'
+    geocoder = OpenCageGeocode(key)
+
+    results = geocoder.reverse_geocode(latitude, longitude)
+    pprint(results)
+
+coordinates_to_address(27.1750151,78.0421552)
