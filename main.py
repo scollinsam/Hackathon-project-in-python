1cae37ec23c4268180af3f52aa1844ae1c90e6a5
gabs <gabs.lip@gmail.com>
2019-01-03 10:09:31 +0200

    group page jinja

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import bottle
-from bottle import route, run, template, static_file, post, request, get
+from bottle import route, run, template, static_file, post, request, get, jinja2_view
 import os
 import json
+from functools import partial
 bottle.TEMPLATE_PATH.insert(0, os.getcwd())
 
+# the partial makes it possible to write less verbose decorators to choose the html file
+view = partial(jinja2_view, template_lookup=['templates'])
 info = {}
 
 @route('/')
@@ -16,6 +19,12 @@ def css():
     return static_file("style.css", root='css')
 
 
+@route('/group/group_page.css')
+def css():
+    print("css called")
+    return static_file("group_page.css", root='css')
+
+
 @route('/script.js')
 def js():
     return static_file("script.js", root='js')
@@ -24,6 +33,16 @@ def js():
 def js():
     return static_file("groups.js", root='js')
 
+@route('/group/<name>')
+@view('group_page.html')
+def greet(name='Stranger'):
+    name_r = "Data Science NLP"
+    organiser_r = "Simono"
+    level_r = "Beginner"
+    location_r = "rothschild area"
+    frequency_r = "6h/week"
+    return {'name': name_r, 'organiser': organiser_r, 'level':level_r, 'location':location_r, 'frequency':frequency_r}
+    # return template('Hello {{name}}, how are you?', name=name)
 
 @get('/form_input')
 def send_input():
